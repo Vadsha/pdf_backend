039e735e5c598c908c85931b2d34107b47ac5e10
vaddshah <heinzwe2626@gmail.com>
2023-05-20 15:55:56 +0630

    need customer registers

--- routes/client.php
+++ routes/client.php
@@ -11,5 +11,6 @@ Route::get('/get-book/{book}' , [HomeController::class , 'getBook']);
 Route::get('/get-categories' , [HomeController::class , 'getCategories']);
 Route::get('/get-tags' , [HomeController::class , 'getTags']);
 Route::get('/get-books-by-download' , [HomeController::class , 'BooksByDownload']);
-Route::get('/get-books-by-categories/{categories}' , [HomeController::class , 'booksByCategories']);
+Route::get('/get-books-by-categories/{categories}' , [HomeController::class , 'booksByCategory']);
+Route::get('/get-books-by-tags/{tag}' , [HomeController::class , 'booksByTag']);
 Route::post('/comment' , [HomeController::class , 'storeComment']);
